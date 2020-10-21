d4da9a53a69f0a840dae164fddfaf5826831f29d
huangdongyang <757551888@qq.com>
2020-10-21 22:20:28 +0800

    hdy测试

---
+++ src/main/java/com/qfedu/common/Test.java
@@ -0,0 +1,16 @@
+package com.qfedu.common;
+
+/**
+ * projectName: demo
+ * author: HDY
+ * time: 2020/10/21 22:19
+ * description:
+ */
+public class Test {
+
+
+    public static void main(String[] args) {
+        System.out.println("args = " + args);
+    }
+
+}
