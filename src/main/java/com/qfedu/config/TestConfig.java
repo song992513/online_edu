e27456b94cb89ab6c307cb186ef7c986e6e71524
szc <song992513@163.com>
2020-10-22 19:48:31 +0800

    宋子聪分支的第一次测试

---
+++ src/main/java/com/qfedu/config/TestConfig.java
@@ -0,0 +1,13 @@
+package com.qfedu.config;
+
+/**
+ * projectName: demo
+ *
+ * @author: szc
+ * time:2020/10/22 19:47
+ * description:
+ */
+public class TestConfig {
+    
+    String name = "宋子聪";
+}
